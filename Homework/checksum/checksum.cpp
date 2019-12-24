a635d148b5235afd4e84faab9da259414fe3245e
searchstar <632863986@qq.com>
2019-12-24 17:30:43 +0800

    checksum

--- Homework/checksum/checksum.cpp
+++ Homework/checksum/checksum.cpp
@@ -6,7 +6,21 @@ uint16_t be16(const uint8_t* a) {
     return ((uint16_t)a[0] << 8) | a[1];
 }
 uint16_t checksum(uint8_t* packet, size_t len) {
-    
+    uint32_t sum = 0;
+    len >>= 1;
+    for (int i = 0; i < 5; ++i) {
+        sum += be16(packet);
+        packet += 2;
+    }
+    packet += 2;
+    for (int i = 6; i < len; ++i) {
+        sum += be16(packet);
+        packet += 2;
+    }
+    while (sum >> 16) {
+        sum = (sum & 0xffff) + (sum >> 16);
+    }
+    return ~sum;
 }
 /**
  * @brief 进行 IP 头的校验和的验证
@@ -18,7 +32,6 @@ bool validateIPChecksum(uint8_t *packet, size_t packet_len) {
   // TODO:
   //size_t head_len = *packet >> 4;
   size_t head_len = (*packet & 0xf) * 4;
-  //printf("head_len = %lu\n", head_len);
   if (head_len > packet_len) {
       return false;
   }
